ce316330d379cc0f6c3659dda00a12ad9d071cbd
gongshuo <470690988@qq.com>
2020-07-29 16:27:51 +0800

    this is all

--- Helloworld/Helloworld/urls.py
+++ Helloworld/Helloworld/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$', views.hello),
+    # path('hello/', views.hello),
 ]
